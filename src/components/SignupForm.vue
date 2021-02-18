35ea4aa6cfb47de49d35c27f4811ab9b297de914
Vinay Poduval <vinaypoduval@hotmail.com>
2021-02-18 16:22:47 +0530

    Send data to autocode endpoint

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -3,7 +3,13 @@
         <h1 class="text-white text-center">Fill Form</h1>
     </div>
     <div class="container bg-light p-3">
-        <form id="form" @submit="handleSubmit">
+        <div v-if="loading">
+            <h2>Submitting Form</h2>
+            <div class="text-center p-3">
+                <div class="spinner-border"></div>
+            </div>
+        </div>
+        <form v-if="!loading" id="form" @submit="handleSubmit">
             <div class="form-group">
                 <label for="name">Name:</label>
                 <input type="text" class="form-control" id="name" placeholder="Enter name.." required v-model="name">
@@ -15,17 +21,17 @@
             <div class="form-group">
                 <label for="contact">Contact No.:</label>
                 <input type="tel" class="form-control" id="phone" placeholder="Enter contact no. .." required v-model="phone">
-                <div class="error" v-if="phoneErr">
+                <!--<div class="error" v-if="phoneErr">
                     <p>{{phoneErr}}</p>
-                </div>
+                </div>-->
             </div>
             <div class="form-group">
                 <label for="role">Applying for:</label>
                 <select class="form-control" v-model="role">
                     <option value="default">Choose role</option>
-                    <option value="developer">Web Developer</option>
-                    <option value="tester">Software Tester</option>
-                    <option value="designer">Web Designer</option>
+                    <option value="Web Developer">Web Developer</option>
+                    <option value="Software Tester">Software Tester</option>
+                    <option value="Web Designer">Web Designer</option>
                 </select>
             </div>
             <div class="text-center p-3">
@@ -36,22 +42,50 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
     data() {
         return {
             name: '',
             email: '',
             phone: '',
-            phoneErr: '',
-            role: 'default'
+            role: 'default',
+            loading: false
         }
     },
     methods: {
         handleSubmit(e) {
             e.preventDefault()
-
-            this.phone.length === 10 ? 
-                '' : this.phoneErr = '*Contact no. should be 10 digits'
+            this.loading = true
+            const name = this.name
+            const email = this.email
+            const phone = this.phone
+            const role = this.role
+            const newData = {
+                name,
+                email,
+                phone,
+                role
+            }
+            this.sendData(newData)
+        },
+        async sendData(newData) {
+            try {
+                const res = await axios.post('https://wanom.api.stdlib.com/vue-form@dev/', {newData})
+                if(res.status === 200) {
+                    this.name = this.email = this.phone = '' 
+                    this.role = 'default'
+                    this.loading = false
+                    alert('Form submitted successfully')
+                }
+                else {
+                    alert(`There was an error ${res.status}`)
+                }
+            }
+            catch(e) {
+                console.error(e)
+            }
         }
     }
 }
